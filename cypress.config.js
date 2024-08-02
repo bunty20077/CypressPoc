88ba0373a6cac942ea2f7be543ad9c843d8394a5
Sayantan Tarafdar <sayantan20077@rediffmail.com>
2024-08-02 18:57:44 +0530

    Environment Parameterization done

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,24 @@
-const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+
+const environment = process.env.CYPRESS_ENV || 'development';
+
+const config = {
+  development: {
+    baseUrl: 'https://pokeapi.co/api/v2/pokemon',
+    apiUrl: 'https://pokeapi.co/api/v2/pokemon'
+  },
+  staging: {
+    baseUrl: 'https://staging.example.com',
+    apiUrl: 'https://staging.example.com/api'
+  },
+  production: {
+    baseUrl: 'https://example.com',
+    apiUrl: 'https://example.com/api'
+  }
+};
+
+module.exports = ({
+  env: config[environment],
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -4,21 +4,21 @@ describe('REST API TEST WITH CYPRESS', () =>{
 
     it('API Test - Header Validation', () =>{
 
-        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.request(Cypress.env('baseUrl')).as('pokemon')
         cy.get('@pokemon').its('headers').its('content-type')
         .should('include','application/json; charset=utf-8') // Verifying Header    
     })
 
     it('API Test - Status Validation', () =>{
 
-        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.request(`${Cypress.env('baseUrl')}/25`).as('pokemon')
         cy.get('@pokemon').its('status')
         .should('equal',200) // Verifying Response Status Code   
     })
 
     it('API Test - Validate Name Value', () => {
 
-        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.request(`${Cypress.env('baseUrl')}/25`).as('pokemon')
         cy.get('@pokemon').its('body')
         .should('include',{name:'pikachu'}) // Verifying Response Name Value  
         //.should('have.property','name','pikachu') // Both the process is correct
