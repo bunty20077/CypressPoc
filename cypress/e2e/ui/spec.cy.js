c6d8c03732719259a55a9dd401b22943cfc8e169
Sayantan Tarafdar <sayantan20077@rediffmail.com>
2024-08-21 15:21:42 +0530

    Introduction of cypress fixtures

--- cypress/e2e/ui/spec.cy.js
+++ cypress/e2e/ui/spec.cy.js
@@ -2,17 +2,27 @@
 import LoginPage from "../../pageobjects/loginPage";
 
 
-describe('Poke UI Automation ', () => {
-  it('Entering data into form', () => {
+describe('Para Bank Automation ', () => 
 
-    const loginPage = new LoginPage;
-    cy.visit(Cypress.env('uiUrl')).as('pokemon')
-    loginPage.getUsernameField().clear();
-    loginPage.getUsernameField().type('supereasy');
-    loginPage.getPasswordField().type('supereasy');
-    loginPage.getLoginBtn().click();
-    cy.wait(2000);
-    cy.screenshot();
+  {
+    let data ;
+    before(function(){  
+      cy.fixture('example').then(function(fdata)
+      {
+          data = fdata
+
+      })
+    })
+    it('Entering data into form', () => {
+
+      const loginPage = new LoginPage;
+      cy.visit(Cypress.env('uiUrl')).as('para bank website')
+      loginPage.getUsernameField().clear();
+      loginPage.getUsernameField().type(data.username);
+      loginPage.getPasswordField().type(data.password);
+      loginPage.getLoginBtn().click();
+      cy.wait(2000);
+      cy.screenshot();
 
    
   })
